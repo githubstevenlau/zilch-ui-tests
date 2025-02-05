b63f4b093b22ca89535601479fcfa0894af1908c
Steven Lau <softwaredev@stevenlau.net>
2025-02-05 16:34:23 +0000

    Add class for login tests.

--- src/test/java/zilch/login/base/BaseTest.java
+++ src/test/java/zilch/login/base/BaseTest.java
@@ -1,16 +1,24 @@
 package zilch.login.base;
 
+import com.github.javafaker.Faker;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+
 import zilch.pages.BasePage;
 import zilch.pages.LoginPage;
 
-import java.util.concurrent.TimeUnit;
-
 import static utilities.Utility.setUtilityDriver;
+import static utilities.Utility.getRandomString;
+
 
 public class BaseTest {
 
@@ -18,6 +26,31 @@ public class BaseTest {
     protected BasePage basePage;
     protected LoginPage loginPage;
     private final String url = "https://customers.payzilch.com/login";
+
+    Faker faker = new Faker(new Locale("en-GB"));
+
+    /*
+     * Data provider for fake emails
+     */
+    @DataProvider(name = "emailAddress")
+    public Object[][] createFakeEmail() {
+        return new Object[][] {
+                { faker.internet().emailAddress() },
+                { faker.internet().emailAddress() }
+        };
+    }
+
+    /*
+     * Data provider for random string - can be used to create a parameterised
+     * test that can be used for boundary value testing or input/output partitioning.
+     */
+    @DataProvider(name = "randomString")
+    public Object[][] createRandomString() {
+        return new Object[][] {
+                { getRandomString(1) },
+                { getRandomString(10 ) }
+        };
+    }
 
     @BeforeClass
     public void setUp() {
@@ -42,5 +75,4 @@ public class BaseTest {
         basePage.setDriver(driver);
         setUtilityDriver();
     }
-
 }
