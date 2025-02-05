f34fba743beaaaa58feff035a0dc3ecffa1fc50c
Steven Lau <softwaredev@stevenlau.net>
2025-02-05 16:35:10 +0000

    Add tests for login fuctionality.

--- src/test/java/zilch/login/tests/LoginTests.java
+++ src/test/java/zilch/login/tests/LoginTests.java
@@ -1,16 +1,27 @@
 package zilch.login.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import zilch.login.base.BaseTest;
 
 public class LoginTests extends BaseTest {
 
-    @Test
-    public void loginErrorMessageTest() {
-        loginPage.setEmail("invalid@email.com");
+    @Test (dataProvider = "emailAddress")
+    public void loginInvalidEmailTest(String emailAddress) {
+        loginPage.setEmail(emailAddress);
         loginPage.setPassword("password123!");
 
         loginPage.clickLoginButton();
+    }
+
+    @Test (dataProvider = "randomString")
+    public void stringLoginTest(String randomString) {
+        loginPage.setEmail(randomString);
+        loginPage.setPassword("password123!");
+
+        loginPage.clickLoginButton();
+
+        String validationMessage = loginPage.getInvalidEmailValidationMessage();
+        Assert.assertTrue(validationMessage.contains("Please enter a valid email address"));
     }
 }
